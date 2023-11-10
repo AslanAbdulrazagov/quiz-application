efd691027709edce968c22c64a3f4ee14b9abe82
AslanAbdulrazagov <aslanabdulrazaqov496@gmail.com>
2023-11-10 12:04:56 +0400

    Application was created

---
+++ quizapp.js
@@ -0,0 +1,411 @@
+var array = [
+    {
+      question: "5 %-i 2 olan ədədi tap?",
+      A: "A)43",
+      B: "B)40",
+      C: "C)49",
+      D: "D)35",
+      correctvariant: "B)40",
+      subject: "Riyaziyyat",
+      level: "Asan",
+    },
+    {
+      question: "(x+3)+(x+1)=12 Tənliyi həll edin?",
+      A: "A)3",
+      B: "B)5",
+      C: "C)4",
+      D: "D)35",
+      correctvariant: "C)4",
+      subject: "Riyaziyyat",
+      level: "Asan",
+    },
+    {
+      question: "Sürəti 7 olan neçə düzgün olmayan kəsr var?",
+      A: "A)7",
+      B: "B)32",
+      C: "C)4",
+      D: "D)6",
+      correctvariant: "A)7",
+      subject: "Riyaziyyat",
+      level: "Asan",
+    },
+    {
+      question: "4%-i 5,6 olan ədədi tapın:",
+      A: "A)48",
+      B: "B)400",
+      C: "C)49",
+      D: "D)140",
+      correctvariant: "D)140",
+      subject: "Riyaziyyat",
+      level: "Asan",
+    },
+    {
+      question: "0,1(3) sonsuz dövrü onluq kəsrini adi kəsrə çevirin.",
+      A: "A)2/15",
+      B: "B)26/15",
+      C: "C)12/15",
+      D: "D)7/15",
+      correctvariant: "A)2/15",
+      subject: "Riyaziyyat",
+      level: "Asan",
+    },
+    {
+      question:
+        "İki ədədin cəmi 5, fərqi 3-dür.Bu ədədlərin hasilini tapın?",
+      A: "A)4",
+      B: "B)7",
+      C: "C)9",
+      D: "D)5",
+      correctvariant: "A)4",
+      subject: "Riyaziyyat",
+      level: "Orta",
+    },
+    {
+      question: "X≠0 olarsa, 2x(x+y)=5y,x(x-y)=y olduqda x-i tapın?",
+      A: "A)3/4",
+      B: "B)5",
+      C: "C)4/9",
+      D: "D)38",
+      correctvariant: "A)3/4",
+      subject: "Riyaziyyat",
+      level: "Orta",
+    },
+    {
+      question:
+        "x+y=5,x-y=1 olduğuna görə, x2-y2+3x+3y ifadəsinin qiymətini tapın?",
+      A: "A)10",
+      B: "B)20",
+      C: "C)30",
+      D: "D)40",
+      correctvariant: "B)20",
+      subject: "Riyaziyyat",
+      level: "Orta",
+    },
+    {
+      question: "2x+y=5,x-3y=-5 tənliklər sistemindən x+y cəmini tapın?",
+      A: "A)4",
+      B: "B)7",
+      C: "C)3",
+      D: "D)5",
+      correctvariant: "C)3",
+      subject: "Riyaziyyat",
+      level: "Orta",
+    },
+    {
+      question: "x+3=2(y-4),y-2=4x tənliklər sistemindən x-i tapın:",
+      A: "A)1/15",
+      B: "B)2/15",
+      C: "C)3/15",
+      D: "D)4/15",
+      correctvariant: "B)2/15",
+      subject: "Riyaziyyat",
+      level: "Orta",
+    },
+    {
+      question: "Hesablayın: (9-2√10)(9+2√10)?",
+      A: "A)36",
+      B: "B)12",
+      C: "C)12√10",
+      D: "D)41",
+      correctvariant: "D)41",
+      subject: "Riyaziyyat",
+      level: "Cetin",
+    },
+    {
+      question: "Hesablayın: (3√5+√15)2-10√27?",
+      A: "A)80",
+      B: "B)70",
+      C: "C)60",
+      D: "D)50",
+      correctvariant: "C)60",
+      subject: "Riyaziyyat",
+      level: "Cetin",
+    },
+    {
+      question: "√(5+2√6)=?",
+      A: "A)√2+√6",
+      B: "B)√3+√2",
+      C: "C)√5+√2",
+      D: "D)√3+√5",
+      correctvariant: "B)√3+√2",
+      subject: "Riyaziyyat",
+      level: "Cetin",
+    },
+    {
+      question: "A=√6+1 və b=√6 -1 olduğuna görə, a:b+b:a=?",
+      A: "A)2,8",
+      B: "B)3,8",
+      C: "C)3",
+      D: "D)2",
+      correctvariant: "A)2,8",
+      subject: "Riyaziyyat",
+      level: "Cetin",
+    },
+    {
+      question: "(x-3)2+(y+7)2=0 isə, xy=?",
+      A: "A)23",
+      B: "B)-32",
+      C: "C)21",
+      D: "D)-21",
+      correctvariant: "D)-21",
+      subject: "Riyaziyyat",
+      level: "Cetin",
+    },
+    {
+      question: "Azərbaycan dilində neçə sait var?",
+      A: "A)8",
+      B: "B)9",
+      C: "C)7",
+      D: "D)11",
+      correctvariant: "B)9",
+      subject: "Az/dili",
+      level: "Asan",
+    },
+    {
+      question: "Azərbaycan dilində neçə samit səs var?",
+      A: "A)34",
+      B: "B)23",
+      C: "C)25",
+      D: "D)32",
+      correctvariant: "C)25",
+      subject: "Az/dili",
+      level: "Asan",
+    },
+    {
+      question: "Hansı kar samitin cingiltili qarşılığı yoxdur?",
+      A: "A)H",
+      B: "B)F",
+      C: "C)G",
+      D: "D)C",
+      correctvariant: "A)H",
+      subject: "Az/dili",
+      level: "Asan",
+    },
+    {
+      question: "Əlifbada neçə hərf var?",
+      A: "A)30",
+      B: "B)23",
+      C: "C)32",
+      D: "D)34",
+      correctvariant: "C)32",
+      subject: "Az/dili",
+      level: "Asan",
+    },
+    {
+      question: "Bitişdirici samitləri göstərin:",
+      A: "A)d,t,l",
+      B: "B)n,m,s",
+      C: "C)m,l,k",
+      D: "D)n,y,s",
+      correctvariant: "D)n,y,s",
+      subject: "Az/dili",
+      level: "Asan",
+    },
+    {
+      question: "Qrammatik cəhətdən feil bildirir?",
+      A: "A)Hərəkət",
+      B: "B)Xususiyyət",
+      C: "C)Keyfiyyət",
+      D: "D)Əlamət",
+      correctvariant: "A)Hərəkət",
+      subject: "Az/dili",
+      level: "Orta",
+    },
+    {
+      question: "Feilin neçə sadə şəkli var?",
+      A: "A)4",
+      B: "B)5",
+      C: "C)6",
+      D: "D)8",
+      correctvariant: "C)6",
+      subject: "Az/dili",
+      level: "Orta",
+    },
+    {
+      question: "Feil haqqında hansı fikir səhvdir?",
+      A: "A)feilin dərəcələri var",
+      B: "B)feil hərəkət bildirir",
+      C: "C)feil təsirli və təsirsiz olur",
+      D: "D)feilin 3 zamanı var",
+      correctvariant: "A)feilin dərəcələri var",
+      subject: "Az/dili",
+      level: "Orta",
+    },
+    {
+      question: "Feilin quruluşca neçə novu var?",
+      A: "A)5",
+      B: "B)6",
+      C: "C)4",
+      D: "D)3",
+      correctvariant: "D)3",
+      subject: "Az/dili",
+      level: "Orta",
+    },
+    {
+      question: "Feil cümlədə əsasən hansı cümlə üzvü olur?",
+      A: "A)təyin",
+      B: "B)mübtəda",
+      C: "C)xəbər",
+      D: "D)zərflik",
+      correctvariant: "C)xəbər",
+      subject: "Az/dili",
+      level: "Orta",
+    },
+    {
+      question: "Hansı əlaqə növü bütün tabeli mürəkkəb cümlələrdə olur?",
+      A: "A)Qarşılaşdırma",
+      B: "B)Zaman",
+      C: "C)Səbəb-nəticə",
+      D: "D)Heç biri",
+      correctvariant: "B)Zaman",
+      subject: "Az/dili",
+      level: "Cetin",
+    },
+    {
+      question: "Mürəkkəb cümlənin hansı növləri var?",
+      A: "A)tabeli, tabesiz",
+      B: "B)düzəltmə",
+      C: "C)əmr",
+      D: "D)sadə",
+      correctvariant: "A)tabeli, tabesiz",
+      subject: "Az/dili",
+      level: "Cetin",
+    },
+    {
+      question: "Mürəkkəb cümlənin neçə novu var?",
+      A: "A)6",
+      B: "B)2",
+      C: "C)5",
+      D: "D)3",
+      correctvariant: "D)3",
+      subject: "Az/dili",
+      level: "Cetin",
+    },
+    {
+      question: "Hansı bağlayıcı merəkkəb cümlənin tərəflərini bağlamır?",
+      A: "A)ancaq",
+      B: "B)ilə",
+      C: "C)belə ki",
+      D: "D)yəni",
+      correctvariant: "B)ilə",
+      subject: "Az/dili",
+      level: "Cetin",
+    },
+    {
+      question: "Mürəkkəb cümlələr neçə növə ayrılır?",
+      A: "A)2",
+      B: "B)4",
+      C: "C)5",
+      D: "D)3",
+      correctvariant: "A)2",
+      subject: "Az/dili",
+      level: "Cetin",
+    },
+  ];
+  // console.log(array.filter(x=>x.subject=="Az/dili" &&x.level=="Cetin"))
+  var fenn='',alevel='',duz_cavab=0,sehv_cavab=0,cvb=0,sual_sayi=0;
+  let i=0,f=[];
+
+  a = document.querySelectorAll(".fenn_div");
+  a.forEach(element => {
+    element.addEventListener("click", function () {
+      document.querySelector("#first_page").style.display = "none";
+      document.querySelector("#second_page").style.display = "flex";
+      fenn=element.innerHTML;
+      
+    });
+    
+  });
+  b = document.querySelectorAll(".seviyye");
+  b.forEach(element => {
+    element.addEventListener("click", function () {
+      document.querySelector("#second_page").style.display = "none";
+      document.querySelector("#seviyye_modal").style.display = "flex";
+      alevel=element.children[0].innerHTML;
+     Quizz(i)
+      
+    });
+    
+  });
+  var m;
+  function Quizz(index) {
+   
+
+    f=array.filter(x=>x.subject==fenn && x.level==alevel);
+
+    document.querySelector("#seviyye_modal_container").innerHTML=`<div class="basliq">
+      <span id="say">${index+1}.</span><span id="sual">${(f[index]).question}</span>
+    </div>
+    <div class="cavablar">
+      <button class="cavab" id="a">${f[index].A}</button>
+      <button class="cavab" id="b">${f[index].B}</button>
+      <button class="cavab" id="c">${f[index].C}</button>
+      <button class="cavab" id="d">${f[index].D}</button>
+    </div>
+    <div class="click">
+      <i class="fa-solid fa-arrow-left" id="prew"></i>
+      <button class="sehife">1</button>
+      <button class="sehife">2</button>
+      <button class="sehife">3</button>
+      <button class="sehife">4</button>
+      <button class="sehife">5</button>
+      <i class="fa-solid fa-arrow-right" style="margin-left: 20px" id="next" onclick="Nextt(${index})"></i>
+
+    </div>`
+
+    m=document.querySelectorAll(".cavab");
+    m.forEach((z,i) => {
+      z.addEventListener("click", function () {
+        if(z.innerHTML==f[index].correctvariant){
+          z.style.background="green"
+          duz_cavab++;
+          
+      }
+      else{
+        z.style.background="red"
+        sehv_cavab++;
+      }
+       m.forEach(s=>{
+        if(s.innerHTML==f[index].correctvariant){
+          s.style.background="green"
+      }
+      s.setAttribute("disabled","")
+       })
+
+       dogru.innerHTML=duz_cavab
+       yanlis.innerHTML=sehv_cavab
+       cevapsiz.innerHTML=f.length-(duz_cavab+sehv_cavab)
+       umumi.innerHTML=f.length
+      
+
+
+    })
+    
+  });
+ 
+
+
+    c=document.querySelectorAll(".sehife");
+
+  
+  c.forEach((element,index) => {
+    
+    element.addEventListener("click",function () {
+     
+     Quizz(index);
+    })
+  });
+  
+  }
+  function Nextt(x) {
+    if(x==4){
+      document.querySelector("#seviyye_modal").style.display="none";
+      document.querySelector("#netice").style.display="flex";
+    }
+    Quizz(x+1)
+
+  } 
+  
+  
+ 
+
